42f883d8e515ee7dfb7888cbf2aff6cdb0a29289
Peter Holmes <ppholmes@usc.edu>
2022-07-13 16:52:20 -0700

    Work please?

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -8,27 +8,27 @@
     
     <main>
         <div class="relative bg-black flex">
-            <div class="relative bg-[url('/katybooproductions/images/bg3.jpg')] bg-no-repeat opacity-50 blur-sm bg-cover w-full h-96">
+            <div class="relative bg-[url('/images/bg3.jpg')] bg-no-repeat opacity-50 blur-sm bg-cover w-full h-96">
             </div>
             <h1 class="absolute bottom-0 inset-x-0 text-5xl text-white font-serif p-8">It starts with listening</h1>
             <div class="absolute inset-y-0 right-0 w-1/4 m-10">
-                <img src="/katybooproductions/images/hs1.jpg" alt="" class="rounded-full max-h-72 opacity-100">
+                <img src="/images/hs1.jpg" alt="" class="rounded-full max-h-72 opacity-100">
             </div>
         </div>
         <div class="p-8 md:px-32 text-xl">
             A good story should be <strong>emotional</strong> and <strong>credible</strong>; create a <strong>connection</strong> and <strong>impact</strong> the audience.<br><br>
             Authentic stories move the needle. They inspire action. But I've learned that before you can be a good story teller, you need to be a good <strong>listener</strong>.<br><br>
             I listen to the needs and the nuances that make each client unique. I craft stories based on experiences and expectations. This individualized approach helps form and shape each distinct and remarkable story.<br><br>
-            <img src="/katybooproductions/images/bg4.jpg" alt="">
+            <img src="/images/bg4.jpg" alt="">
             <br>
             I've written, directed and produced for major corporations and local businesses, as well as contributed to national TV programs.<br><br> 
             I've managed local crews and international fixers, all on deadline while working within a budget. <br><br>
             <div class="w-full flex justify-center">
-                <img src="/katybooproductions/images/logos.png" alt="" class="max-h-96">
+                <img src="/images/logos.png" alt="" class="max-h-96">
             </div>
         </div>
         <div class="bg-bggray flex flex-col md:flex-row content-center gap-8 px-8 md:px-32">
-            <div class="h-64 md:w-3/4 my-8 bg-[url('/katybooproductions/images/hs2.jpg')] bg-contain bg-center bg-no-repeat">
+            <div class="h-64 md:w-3/4 my-8 bg-[url('/images/hs2.jpg')] bg-contain bg-center bg-no-repeat">
             </div>
             <div class="text-2xl content-center py-2 md:py-8">
                 I worked at KSTP-TV in the Twin Cities where I reported live everyday, covering all news including local and national, the crime beat, and of course, the Minnesota State Fair.<br><br>
@@ -41,7 +41,7 @@
             I live in the Twin Cities.  You might find me walking around one of the lakes.  Or working on my latest project: hand-painting cool signs on century-old reclaimed barnwood. <a class="underline" href="https://www.reclaimedwords.com">reclaimedwords.com</a> <br><br>
         </div>
         <div class="relative">
-            <div class="bg-[url('/katybooproductions/images/bg5.jpg')] opacity-50 blur-sm py-56 bg-center bg-no-repeat bg-cover"></div>
+            <div class="bg-[url('/images/bg5.jpg')] opacity-50 blur-sm py-56 bg-center bg-no-repeat bg-cover"></div>
             <h1 class="absolute top-0 py-16 px-8 md:px-16 lg:px-32 text-4xl font-serif flex justify-start w-full">
                 What happens when you pay attention.
             </h1>
@@ -52,6 +52,6 @@
         <div class="bg-titlegray py-20 px-16">
             <h1 class="flex justify-center font-bold text-center w-full text-white font-serif text-5xl">Let's talk about how I can help with your next project.</h1>
             <hr class="my-8 text-bggold">
-            <button class="font-sans text-2xl w-full text-white hover:text-bggold hover:bg-white p-4 border-2" href="/katybooproductions">Contact</button>
+            <button class="font-sans text-2xl w-full text-white hover:text-bggold hover:bg-white p-4 border-2" href="/">Contact</button>
         </div>
     </main>
