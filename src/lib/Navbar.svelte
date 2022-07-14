218e6eaec861ee725037752b8141b890d94ea474
Peter Holmes <ppholmes@usc.edu>
2022-07-13 18:22:06 -0700

    Added title

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -16,7 +16,7 @@
 
 </script>
 
-<div class="static flex w-full md:sticky top-0 overflow-x-clip px-4 flex-col md:flex-row  bg-white justify-center align-center md:justify-start z-10">
+<div class="static flex w-full md:sticky top-0 overflow-x-clip px-4 flex-col md:flex-row bg-white justify-center align-center md:justify-start z-10">
     <nav class=" m-auto">
         <div class="flex w-full px-5 m-auto box-border items-center h-full">
             <ul class="flex flex-col md:flex-row">
